e8a9f4191f6d1c1029d9f13af96f63118d0c6b97
Adrian Jasaroska <adrianjasaroska@gmail.com>
2026-06-06 08:47:25 +0200

    Files moved

---
+++ M319-S44/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Uebung1.ausfuehren(); // Übung 1
+        Uebung2.ausfuehren(); // Übung 2
+        Uebung3.ausfuehren(); // Übung 3
+        Uebung4.ausfuehren(); // Übung 4
+        Uebung5.ausfuehren(); // Übung 5
+    }
+}
